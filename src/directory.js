c8656b4f37ce4664862a7b45f5b7db3cdb5f6f13
lilyhlou <lilylou16@gmail.com>
2020-07-20 10:54:12 -0400

    Added env variables

--- src/directory.js
+++ src/directory.js
@@ -11,23 +11,28 @@ import Navigation from "./navigation.js";
 class Directory extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {apiResponse: ""};
+		this.state = {
+			rtcMembers: [], 
+			inputValue: '',
+		};
+
 	}
 	callAPI() {
 		fetch("http://localhost:9000/testAPI")
 		.then(res => res.text())
-		.then(res => this.setState({ apiResponse: res}));
+		.then(res => this.setState({ rtcMembers: res}));
 	}
 	componentWillMount() {
 		this.callAPI();
 	}
+	
 	render() {
 	return(
 		<div id="directory">
-		<h1>Search for Student Profiles</h1>
-		<p className="App-intro">{this.state.apiResponse}</p>
+		<h1>Search for RTC Member Profiles</h1>
+		<p className="App-intro">{this.state.rtcMembers}</p>
 		<Form inline id="search">
-      		<FormControl type="text" placeholder="RTC Member" id="bar" size="lg"/>
+      		<FormControl type="text" placeholder="RTC Member" id="bar" size="lg" value={this.props.inputValue} onChange={this.props.memberFilterOnChange}/>
       		<Button variant="outline-dark" size="lg" id="button">Search</Button>{' '}
      	 </Form>
      	 </div>
